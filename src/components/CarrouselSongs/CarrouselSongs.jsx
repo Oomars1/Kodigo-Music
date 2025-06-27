467d6b58c32fb6b514a64bcbb0db7cd9de9a2369
Edwin Mendez <mp19034@ues.edu.sv>
2025-06-26 19:42:53 -0600

    se Avanzo en la pagina web

--- src/components/CarrouselSongs/CarrouselSongs.jsx
+++ src/components/CarrouselSongs/CarrouselSongs.jsx
@@ -1,59 +1,126 @@
-
-
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Grid } from 'swiper/modules'; // importante
-import 'swiper/css';
-import 'swiper/css/grid';
-import './CarrouselSongs.css'; // tu CSS
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Grid } from "swiper/modules"; // importante
+import "swiper/css";
+import "swiper/css/grid";
+import "./CarrouselSongs.css"; // tu CSS
 
 const novedades = [
-  { title: "Papasito", autor: "KAROL G", image: "https://elavancemedia.com/wp-content/uploads/2025/06/karol-g-papacito-video.webp" },
-  { title: "Espresso", autor: "Sabrina Carpenter", image: "https://i.scdn.co/image/ab67616d0000b273659cd4673230913b3918e0d5" },
-  { title: "Contigo", autor: "KAROL G & Tiësto", image: "https://i1.sndcdn.com/artworks-m6g23NWDcNIS3adl-71Ccfg-t500x500.jpg" },
-  { title: "Santa", autor: "Lenny Tavárez", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQj7CJ89mv_Ua4RQ44UktnrDG6DIr_esaw_XQ&s" },
-  { title: "Puntería", autor: "Shakira & Cardi B", image: "https://upload.wikimedia.org/wikipedia/en/thumb/b/bf/Shakira_and_Cardi_B_-_Punter%C3%ADa.jpg/250px-Shakira_and_Cardi_B_-_Punter%C3%ADa.jpg" },
-  { title: "Gatita Gangster", autor: "Villano Antillano & Álvaro Díaz", image: "https://i1.sndcdn.com/artworks-Ylye82RfHYcFE1xu-olw61A-t500x500.jpg" },
-  { title: "Yandel 150", autor: "Yandel & Feid", image: "https://cdn-images.dzcdn.net/images/cover/27abf696d0605c653b11b809dacd7a3c/500x500.jpg" },
-  { title: "El Amor de Mi Vida", autor: "La Adictiva", image: "https://i.scdn.co/image/ab67616d0000b27311b77a8693ac5cce83870491" },
-  { title: "Luna", autor: "Feid & ATL Jacob", image: "https://i1.sndcdn.com/artworks-JpSsYnr6AKfg-0-t500x500.jpg" },
-  { title: "Bandido", autor: "Myke Towers & Juhn", image: "https://cdn-images.dzcdn.net/images/cover/e08eb11580eee9678dd97ccad39b8707/0x1900-000000-80-0-0.jpg" },
-  { title: "Mon Amour", autor: "Jhayco & Anitta", image: "https://www.lahiguera.net/musicalia/artistas/varios/disco/0/tema/25687/zzoilo_mon_amour_remix-portada.jpg" },
-  { title: "LALA", autor: "Myke Towers", image: "https://cdn-images.dzcdn.net/images/cover/c9e44eae3791dd967caedc538696887b/1900x1900-000000-80-0-0.jpg" },
-  { title: "Amor Genuino", autor: "Ozuna", image: "https://i.scdn.co/image/ab67616d0000b273845eec3797eb8ca6494988d0" },
-  { title: "Bailé Con Mi Ex", autor: "KAROL G", image: "https://i.scdn.co/image/ab67616d0000b2734dfc44becf424b1571986068" },
-  { title: "Despechá", autor: "ROSALÍA", image: "https://image.europafm.com/clipping/cmsimages02/2022/07/28/5C9F82FB-6A1E-456C-BD0E-13042249FD92/rosalia-estrena-despecha-nueva-cancion_104.jpg?crop=1432,1432,x700,y0&width=1200&height=1200&optimize=low&format=webply" },
-  { title: "No Te Enamores (Remix)", autor: "Nicky Jam & Jhayco", image: "https://i1.sndcdn.com/artworks-n9DajyJK9IabIDEZ-yVpyqg-t500x500.jpg" }
+  {
+    title: "Papasito",
+    autor: "KAROL G",
+    image:
+      "https://elavancemedia.com/wp-content/uploads/2025/06/karol-g-papacito-video.webp",
+  },
+  {
+    title: "Espresso",
+    autor: "Sabrina Carpenter",
+    image: "https://i.scdn.co/image/ab67616d0000b273659cd4673230913b3918e0d5",
+  },
+  {
+    title: "Contigo",
+    autor: "KAROL G & Tiësto",
+    image:
+      "https://i1.sndcdn.com/artworks-m6g23NWDcNIS3adl-71Ccfg-t500x500.jpg",
+  },
+  {
+    title: "Santa",
+    autor: "Lenny Tavárez",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQj7CJ89mv_Ua4RQ44UktnrDG6DIr_esaw_XQ&s",
+  },
+  {
+    title: "Puntería",
+    autor: "Shakira & Cardi B",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/b/bf/Shakira_and_Cardi_B_-_Punter%C3%ADa.jpg/250px-Shakira_and_Cardi_B_-_Punter%C3%ADa.jpg",
+  },
+  {
+    title: "Gatita Gangster",
+    autor: "Villano Antillano & Álvaro Díaz",
+    image:
+      "https://i1.sndcdn.com/artworks-Ylye82RfHYcFE1xu-olw61A-t500x500.jpg",
+  },
+  {
+    title: "Yandel 150",
+    autor: "Yandel & Feid",
+    image:
+      "https://cdn-images.dzcdn.net/images/cover/27abf696d0605c653b11b809dacd7a3c/500x500.jpg",
+  },
+  {
+    title: "El Amor de Mi Vida",
+    autor: "La Adictiva",
+    image: "https://i.scdn.co/image/ab67616d0000b27311b77a8693ac5cce83870491",
+  },
+  {
+    title: "Luna",
+    autor: "Feid & ATL Jacob",
+    image: "https://i1.sndcdn.com/artworks-JpSsYnr6AKfg-0-t500x500.jpg",
+  },
+  {
+    title: "Bandido",
+    autor: "Myke Towers & Juhn",
+    image:
+      "https://cdn-images.dzcdn.net/images/cover/e08eb11580eee9678dd97ccad39b8707/0x1900-000000-80-0-0.jpg",
+  },
+  {
+    title: "Mon Amour",
+    autor: "Jhayco & Anitta",
+    image:
+      "https://www.lahiguera.net/musicalia/artistas/varios/disco/0/tema/25687/zzoilo_mon_amour_remix-portada.jpg",
+  },
+  {
+    title: "LALA",
+    autor: "Myke Towers",
+    image:
+      "https://cdn-images.dzcdn.net/images/cover/c9e44eae3791dd967caedc538696887b/1900x1900-000000-80-0-0.jpg",
+  },
+  {
+    title: "Amor Genuino",
+    autor: "Ozuna",
+    image: "https://i.scdn.co/image/ab67616d0000b273845eec3797eb8ca6494988d0",
+  },
+  {
+    title: "Bailé Con Mi Ex",
+    autor: "KAROL G",
+    image: "https://i.scdn.co/image/ab67616d0000b2734dfc44becf424b1571986068",
+  },
+  {
+    title: "Despechá",
+    autor: "ROSALÍA",
+    image:
+      "https://image.europafm.com/clipping/cmsimages02/2022/07/28/5C9F82FB-6A1E-456C-BD0E-13042249FD92/rosalia-estrena-despecha-nueva-cancion_104.jpg?crop=1432,1432,x700,y0&width=1200&height=1200&optimize=low&format=webply",
+  },
+  {
+    title: "No Te Enamores (Remix)",
+    autor: "Nicky Jam & Jhayco",
+    image:
+      "https://i1.sndcdn.com/artworks-n9DajyJK9IabIDEZ-yVpyqg-t500x500.jpg",
+  },
 ];
 
-  
-
-export function GridCarousel({
-  spaceData,
-  columnaVisible,
-  columnas
-}) {
+export function GridCarousel({ spaceData, columnaVisible, columnas }) {
   return (
     <Swiper
       modules={[Grid]}
       spaceBetween={spaceData}
-      width={500}
       slidesPerView={columnaVisible} // 1 columna visible por vez
       grid={{
         rows: columnas,
-        fill: 'row' // 💡 Esto fuerza el scroll horizontal por columnas
+        fill: "row", // 💡 Esto fuerza el scroll horizontal por columnas
       }}
       grabCursor={true}
     >
       {novedades.map((item, i) => (
-       <SwiperSlide key={i}>
-  <div className="cuadro">
-    <img src={item.image} alt={item.title} className="cuadro-img" />
-    <div className="text-content">
-      <h4>{item.title}</h4>
-      <p>{item.autor}</p>
-    </div>
-  </div>
-</SwiperSlide>
+        <SwiperSlide key={i} className="miSlidePersonalizado">
+          <div className="cuadro">
+            <img src={item.image} alt={item.title} className="cuadro-img" />
+            <div className="text-content">
+              <h4>{item.title}</h4>
+              <p>{item.autor}</p>
+            </div>
+            <span className="menu-icon">⋯</span>
+          </div>
+        </SwiperSlide>
       ))}
     </Swiper>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 // src/pages/Home/Home.jsx
 import React from "react";
-import "./Home.module.css";
+import styles from "./Home.module.css";
 import { GridCarousel } from "../../components/CarrouselSongs/CarrouselSongs";
 import sty from "./MasEscuchado.module.css";
 import style from "./StylesNovedades.module.css";
@@ -8,6 +8,17 @@ import { CarruselGeneral } from "../../components/Carruselgeneral/Carrusel";
 import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
 import NavbarRed from "../../components/NavbarRed/NavbarRed";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faApple } from "@fortawesome/free-brands-svg-icons";
+import {
+  faHome,
+  faCompass,
+  faBook,
+  faRightToBracket,
+} from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+
+
 
 function Home() {
   const masEscuchado = [
@@ -36,6 +47,51 @@ function Home() {
       image:
         "https://is1-ssl.mzstatic.com/image/thumb/Features/v4/3b/9e/af/3b9eafd0-03f8-9fea-04f9-fe30c05ea32e/28456c41-1241-41bf-8351-3658bf8fb8ae.png/480x275sr.webp",
       texto: "El Mediterraneo y el Caribe se encuentran en playas imaginadas",
+    },
+    {
+      title: "NATSUKASHI",
+      autor: "SAIKO",
+      subtitle: "NUEVO ALBUM",
+      image:
+        "https://is1-ssl.mzstatic.com/image/thumb/Features221/v4/1f/b7/de/1fb7de40-1d0f-1e94-6ffd-76eb9096429a/daae4876-6076-406a-98fb-84ecb2260801.png/600x344sr.webp",
+      texto:
+        "Un autorretrato de aire melancolico e influencias transatlanticas",
+    },
+    {
+      title: "NATSUKASHI",
+      autor: "SAIKO",
+      subtitle: "NUEVO ALBUM",
+      image:
+        "https://is1-ssl.mzstatic.com/image/thumb/Features221/v4/1f/b7/de/1fb7de40-1d0f-1e94-6ffd-76eb9096429a/daae4876-6076-406a-98fb-84ecb2260801.png/600x344sr.webp",
+      texto:
+        "Un autorretrato de aire melancolico e influencias transatlanticas",
+    },
+    {
+      title: "NATSUKASHI",
+      autor: "SAIKO",
+      subtitle: "NUEVO ALBUM",
+      image:
+        "https://is1-ssl.mzstatic.com/image/thumb/Features221/v4/1f/b7/de/1fb7de40-1d0f-1e94-6ffd-76eb9096429a/daae4876-6076-406a-98fb-84ecb2260801.png/600x344sr.webp",
+      texto:
+        "Un autorretrato de aire melancolico e influencias transatlanticas",
+    },
+    {
+      title: "NATSUKASHI",
+      autor: "SAIKO",
+      subtitle: "NUEVO ALBUM",
+      image:
+        "https://is1-ssl.mzstatic.com/image/thumb/Features221/v4/1f/b7/de/1fb7de40-1d0f-1e94-6ffd-76eb9096429a/daae4876-6076-406a-98fb-84ecb2260801.png/600x344sr.webp",
+      texto:
+        "Un autorretrato de aire melancolico e influencias transatlanticas",
+    },
+    {
+      title: "NATSUKASHI",
+      autor: "SAIKO",
+      subtitle: "NUEVO ALBUM",
+      image:
+        "https://is1-ssl.mzstatic.com/image/thumb/Features221/v4/1f/b7/de/1fb7de40-1d0f-1e94-6ffd-76eb9096429a/daae4876-6076-406a-98fb-84ecb2260801.png/600x344sr.webp",
+      texto:
+        "Un autorretrato de aire melancolico e influencias transatlanticas",
     },
     {
       title: "NATSUKASHI",
@@ -79,41 +135,130 @@ function Home() {
       texto:
         "Un autorretrato de aire melancolico e influencias transatlanticas",
     },
+    {
+      title: "NATSUKASHI",
+      subtitle: "NUEVO ALBUM",
+      image:
+        "https://is1-ssl.mzstatic.com/image/thumb/Features221/v4/1f/b7/de/1fb7de40-1d0f-1e94-6ffd-76eb9096429a/daae4876-6076-406a-98fb-84ecb2260801.png/600x344sr.webp",
+      texto:
+        "Un autorretrato de aire melancolico e influencias transatlanticas",
+    },
+    {
+      title: "NATSUKASHI",
+      subtitle: "NUEVO ALBUM",
+      image:
+        "https://is1-ssl.mzstatic.com/image/thumb/Features221/v4/1f/b7/de/1fb7de40-1d0f-1e94-6ffd-76eb9096429a/daae4876-6076-406a-98fb-84ecb2260801.png/600x344sr.webp",
+      texto:
+        "Un autorretrato de aire melancolico e influencias transatlanticas",
+    },
+    {
+      title: "NATSUKASHI",
+      subtitle: "NUEVO ALBUM",
+      image:
+        "https://is1-ssl.mzstatic.com/image/thumb/Features221/v4/1f/b7/de/1fb7de40-1d0f-1e94-6ffd-76eb9096429a/daae4876-6076-406a-98fb-84ecb2260801.png/600x344sr.webp",
+      texto:
+        "Un autorretrato de aire melancolico e influencias transatlanticas",
+    },
+    {
+      title: "NATSUKASHI",
+      subtitle: "NUEVO ALBUM",
+      image:
+        "https://is1-ssl.mzstatic.com/image/thumb/Features221/v4/1f/b7/de/1fb7de40-1d0f-1e94-6ffd-76eb9096429a/daae4876-6076-406a-98fb-84ecb2260801.png/600x344sr.webp",
+      texto:
+        "Un autorretrato de aire melancolico e influencias transatlanticas",
+    },
+    {
+      title: "NATSUKASHI",
+      subtitle: "NUEVO ALBUM",
+      image:
+        "https://is1-ssl.mzstatic.com/image/thumb/Features221/v4/1f/b7/de/1fb7de40-1d0f-1e94-6ffd-76eb9096429a/daae4876-6076-406a-98fb-84ecb2260801.png/600x344sr.webp",
+      texto:
+        "Un autorretrato de aire melancolico e influencias transatlanticas",
+    },
+    {
+      title: "NATSUKASHI",
+      subtitle: "NUEVO ALBUM",
+      image:
+        "https://is1-ssl.mzstatic.com/image/thumb/Features221/v4/1f/b7/de/1fb7de40-1d0f-1e94-6ffd-76eb9096429a/daae4876-6076-406a-98fb-84ecb2260801.png/600x344sr.webp",
+      texto:
+        "Un autorretrato de aire melancolico e influencias transatlanticas",
+    },
   ];
 
   return (
     <body>
       <Navbar />
       <NavbarRed />
-      <main className="home">
-        <h1>Novedades</h1>
-        <hr />
-        <CarruselGeneral
-          datos={masEscuchado}
-          imagenPrimero={false}
-          showOverlay={true}
-          styles={style}
-        />
-        {/* <CarrouselNovedades /> */}
-        <h2>Canciones recien llegadas</h2>
-        <hr />
-        <GridCarousel spaceData={5} columnaVisible={2} columnas={4} />
-        <hr />
-        <h2>Todo el mundo esta escuchando...</h2>
-        <hr />
-        {/* <CarruselMasEscuchado /> */}
-        {/* agregar más secciones: carrusel, listas, footer... */}
-        <CarruselGeneral
-          datos={novedades}
-          imagenPrimero={true}
-          showOverlay={false}
-          styles={sty}
-        />
-        <h2>Nuevos Lanzamientos</h2>
-        <hr />
-        <GridCarousel spaceData={3} columnaVisible={2} columnas={2} />
+      <main className={styles.home}>
+        <section className={styles.todo}>
+          <section className={styles.barralateral}>
+            <div className={styles.logo}>
+              <FontAwesomeIcon icon={faApple} />
+              Music
+            </div>
+
+            {/* Buscador */}
+            <div className={styles.search}>
+              <input type="text" placeholder="Buscar..." />
+            </div>
+
+            {/* Navegación */}
+            <ul>
+              <li>
+                <a href="#">
+                  <FontAwesomeIcon icon={faHome} /> Inicio
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <FontAwesomeIcon icon={faCompass} /> Novedades
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <FontAwesomeIcon icon={faBook} /> Radio
+                </a>
+              </li>
+            </ul>
+
+            {/* Botón login */}
+            <Link to="/IniciarSesion" className={styles.loginButton}>
+              <FontAwesomeIcon icon={faRightToBracket} /> Iniciar sesión
+            </Link>
+          </section>
+          <section className={styles.contenido}>
+            <section className={styles.divisor}>
+              <h1>Novedades</h1>
+              <hr />
+              <CarruselGeneral
+                datos={masEscuchado}
+                imagenPrimero={false}
+                showOverlay={true}
+                styles={style}
+              />
+              {/* <CarrouselNovedades /> */}
+              <h2>Canciones recien llegadas</h2>
+              <hr />
+              <GridCarousel spaceData={5} columnaVisible={2} columnas={4} />
+              <hr />
+              <h2>Todo el mundo esta escuchando...</h2>
+              <hr />
+              {/* <CarruselMasEscuchado /> */}
+              {/* agregar más secciones: carrusel, listas, footer... */}
+              <CarruselGeneral
+                datos={novedades}
+                imagenPrimero={true}
+                showOverlay={false}
+                styles={sty}
+              />
+              <h2>Nuevos Lanzamientos</h2>
+              <hr />
+              <GridCarousel spaceData={3} columnaVisible={2} columnas={2} />
+            </section>
+            <Footer />
+          </section>
+        </section>
       </main>
-      <Footer />
     </body>
   );
 }
